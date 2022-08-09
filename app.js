8521a072616bbccc00267e5232fab2d7d16f113b
Katie Brown <katieleebrown19@gmail.com>
2022-08-09 15:25:07 -0400

    created middleware, views, and routes

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const path = require('path')
+const passport = require('passport')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')
+const connectDB = require('./config/db')
+
+
+// Load config
+dotenv.config({ path: './config/config.env' })
+
+// Passport Config
+require('./config/passport')(passport)
+
+// Connect to DB
+connectDB()
+
+// Set up express
+const app = express()
+
+//Logging
+if (process.env.NODE_DEV === 'development') {
+    app.use(morgan('dev'))
+}
+
+//Handlebars
+app.engine('.hbs', exphbs.engine(
+    { extname: '.hbs' }
+));
+app.set('view engine', '.hbs');
+
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI
+    })
+}))
+
+// Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Routes
+app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
+
+//Listen
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
